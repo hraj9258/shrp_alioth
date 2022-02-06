f5bd50cf137f5b0504ac0fe5e197a7839daea7ff
Pranav Talmale <pranavtalmale@gmail.com>
2022-02-06 13:13:01 +0530

    update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,7 @@ DEVICE_PATH := device/xiaomi/alioth
 
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := alioth
+TARGET_OTA_ASSERT_DEVICE := alioth,aliothin
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
